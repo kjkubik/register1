f9b7fe112a1843e70aaaf08f19a869861b4bf6c6
Kim <kkubik7@hotmail.com>
2021-09-03 20:19:45 -0500

    adding error checking branch

--- registration.php
+++ registration.php
@@ -19,19 +19,51 @@ require_once('config.php');
         $lastname    = $_POST['lastname'];
         $phonenumber = $_POST['phonenumber'];
 
-        $sql = "INSERT INTO contact_information (firstname, lastname, phonenumber) VALUES(?,?,?)";
-        $stmtinsert = $db->prepare($sql);
-        $result = $stmtinsert->execute([$firstname, $lastname, $phonenumber]);
+        if (is_numeric($phonenumber) 
+           and preg_match('/[A-Za-z]/', $firstname)
+           and preg_match('/[A-Za-z]/', $lastname)
+           and strlen($firstname) <= 25 
+           and strlen($lastname) <= 25
+           and strlen(strval($phonenumber)) <= 12) {
 
-        if($result){
-            echo 'successfully saved';
-        }else{
-            echo 'error, not saved';
+            $sql = "INSERT INTO contact_information (firstname, lastname, phonenumber) VALUES(?,?,?)";
+            $stmtinsert = $db->prepare($sql);
+            $result = $stmtinsert->execute([$firstname, $lastname, $phonenumber]);
+            
+            if($result){
+                echo 'successfully saved';
+            }else{
+                echo 'error, not saved';
+            }
         }
-        /* TESTING 
-        echo $firstname. " " . $lastname . " " . $phonenumber ;
-        echo 'successfully saved';
-        */
+        else 
+        { 
+            if (!is_numeric($phonenumber)) {
+               echo 'Input numbers for phone number.';
+            } 
+            
+            if (!preg_match('/[A-Za-z]/', $firstname)) {
+            echo 'Input letters only for first name.';
+            }
+
+            if (!preg_match('/[A-Za-z]/', $lastname)) {
+                echo 'Input letters only for last name.';
+            }
+
+            if (strlen($firstname) > 25){
+                echo 'First Name can only be 25 characters.';
+            } 
+
+            if (strlen($lastname) > 25){
+                echo 'Last Name can only be 25 characters.';
+            }
+
+            if (strlen(strval($phonenumber)) > 12) {
+                echo 'Maximum length of phone number is 12.';
+            }
+        }
+
+              
         /* TODO -- I want this echo to be a message by the save button */
     }
     
@@ -57,6 +89,7 @@ require_once('config.php');
 
                     <label for="phonenumber"><b>Phone Number</b></label>
                     <input class="form-control" type="text" name="phonenumber" required>
+
                     <hr class="mb-3">
                     <input class="btn btn-primary" class type="submit" name="create" value="Save">
                     <!-- TODO -- input class="btn btn-primary" class type="submit" name="create" value="Retrieve"-->
@@ -66,6 +99,17 @@ require_once('config.php');
     </form>
 
 </div>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!--script type="text/javascript">
+    $(function(){
+        //alert('hello.');
+        Swal.fire({
+            'title': 'Hello World',
+            'text':  'This is from Sweetalert2',
+            'type':  'Successfully Saved'
+        })
+    });
+</script-->    
 </body>
 </html>
